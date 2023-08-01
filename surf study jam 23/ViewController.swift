0c7f46a71c42cd6fd85798284db1925cae3c3eb8
masnumberone <m.myxortov2011@gmail.com>
2023-08-01 12:08:58 +0300

    Initial Commit

---
+++ surf study jam 23/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  surf study jam 23
+//
+//  Created by work on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
